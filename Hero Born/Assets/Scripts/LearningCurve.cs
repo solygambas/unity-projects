aecaedb56267dafca7aebbc6f68991d0554cc559
solygambas <51904909+solygambas@users.noreply.github.com>
2022-08-08 13:25:53 +0200

    add methods

--- Hero Born/Assets/Scripts/LearningCurve.cs
+++ Hero Born/Assets/Scripts/LearningCurve.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    public int CurrentAge = 30;
+    public int currentAge = 30;
+    public int addedAge = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(30 + 1);
-        Debug.Log(CurrentAge + 1);
+        // variables
+        // Debug.Log(30 + 1);
+        // Debug.Log(currentAge + 1);
+
+        // methods
+        ComputeAge();
+    }
+
+    // method
+    void ComputeAge()
+    {
+        Debug.Log(currentAge + addedAge);
     }
 
     // Update is called once per frame
