d8bdd4d372c7da7f3e724944d958c2ec21329c07
solygambas <51904909+solygambas@users.noreply.github.com>
2022-08-09 08:03:37 +0200

    add readme

--- Hero Born/Assets/Scripts/LearningCurve.cs
+++ Hero Born/Assets/Scripts/LearningCurve.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    private int CurrentAge = 30;
-    // CurrentAge = 30;
-    public int AddedAge = 1;
-    public string FirstName = "Harrison";
+    // private int CurrentAge = 30;
+    // // CurrentAge = 30;
+    // public int AddedAge = 1;
+    
+    // public float Pi = 3.14f;
+    // public string FirstName = "Harrison";
+    // public bool IsAuthor = true;
+
+    // conversions
+    // int MyInteger = 3;
+    // float MyFloat = MyInteger;
+    // int ExplicitConversion = (int)3.14;
+
+    // inferred declaration
+    // var CurrentAge = 32;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +28,37 @@ public class LearningCurve : MonoBehaviour
         // Debug.Log(currentAge + 1);
 
         // methods
-        ComputeAge();
+        // ComputeAge();
+        // Debug.Log($"A string can have variables like {FirstName} inserted directly!");
 
         // debug
-        Debug.Log("Text goes here");
-        Debug.LogFormat("Text goes here, add {0} and {1} as variable placeholders", CurrentAge, FirstName);
+        // Debug.Log("Text goes here");
+        // Debug.LogFormat("Text goes here, add {0} and {1} as variable placeholders", CurrentAge, FirstName);
+
+        int CharacterLevel = 32;
+        // int NextSkillLevel = GenerateCharacter("Spike", CharacterLevel);
+
+        // Debug.Log(NextSkillLevel);
+        // Debug.Log(GenerateCharacter("Spike", CharacterLevel));
+
+        PrintParameter(CharacterLevel);
     }
 
     // method
-    void ComputeAge()
+    // void ComputeAge()
+    // {
+    //     Debug.Log(CurrentAge + AddedAge);
+    // }
+
+    public int GenerateCharacter(string name, int level)
     {
-        Debug.Log(CurrentAge + AddedAge);
+        // Debug.LogFormat("Character : {0} - Level: {1}", name, level);
+        return level += 5;
+    }
+
+    public void PrintParameter(int number)
+    {
+        Debug.Log(number);
     }
 
     // Update is called once per frame
---
+++ Hero Born/README.md
@@ -0,0 +1,7 @@
+# Hero Born
+
+## Features
+
+- setting up a new project in Unity.
+- understanding variables, methods, and classes.
+- formatting code and debugging information.
