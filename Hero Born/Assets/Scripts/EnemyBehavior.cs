54442ec965fa1b8199a1ffcbc36d357b0a079cf1
solygambas <51904909+solygambas@users.noreply.github.com>
2022-08-13 13:49:12 +0200

    add patrol

--- Hero Born/Assets/Scripts/EnemyBehavior.cs
+++ Hero Born/Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+    void Update()
+    {
+        if(_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if(Locations.Count == 0)
+            return;
+
+        _agent.destination = Locations[_locationIndex].position;
+
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
