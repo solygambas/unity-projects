30789aeeb1e0445cdffe60f605bf821bbd449183
solygambas <51904909+solygambas@users.noreply.github.com>
2022-08-13 08:17:57 +0200

    add jumps

--- Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
+
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
 
@@ -12,14 +16,19 @@ public class PlayerBehavior : MonoBehaviour
 
     private Rigidbody _rb;
 
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _isJumping |= Input.GetKeyDown(KeyCode.Space); 
         // up, down, W, S
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         // left, right, A, D
@@ -31,9 +40,23 @@ public class PlayerBehavior : MonoBehaviour
 
     void FixedUpdate()
     {
+        if(IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
+
         Vector3 rotation = Vector3.up * _hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, 
+        QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
